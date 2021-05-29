38904e842147d271aad2167001981f183bb32003
ericamonica <erikalaurindo07@gmail.com>
2021-05-29 10:25:08 -0300

    primeiro commit

---
+++ sprint.js
@@ -0,0 +1,19 @@
+let idade = 33
+if(idade >= 18){
+console.log(" você tem mais de 18 anos :")
+}else{
+    console.log("infelizmente você é menor de idade")
+} 
+let idades = 33
+let iHuman = true
+if((idades >= 18) && (iHuman === true)){
+    console.log('Prabéns você é humana')
+}else{
+    console.log('Você é um robô ')
+}
+let mes= 'maio'
+if(mes == 'janeiro'|| mes =='dezembro'){
+    console.log(' você e de janeiro ou dezembro parabéns')
+}else{
+    console.log('Que pena você não faz aniversário em janeiro e nem dezembro ')
+}
